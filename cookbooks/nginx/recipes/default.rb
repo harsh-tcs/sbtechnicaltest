e5081077da1c98a4d734cbc70bd9bf03775d2f9d
Harsh Chowdhary <iharsh.c@gmail.com>
2016-07-21 13:27:36 +0000

    edited template, Attribute and recipe for Nginx

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -2,7 +2,37 @@
 # Cookbook Name:: nginx
 # Recipe:: default
 #
-# Copyright 2016, YOUR_COMPANY_NAME
+# Copyright 2016, Harsh Chowdhary
 #
 # All rights reserved - Do Not Redistribute
 #
+
+log "Installing Nginx Package"
+
+        package 'nginx' do
+           action :install
+        end
+
+
+log "Writing nginx configuration file for load balancing"	
+	
+	template '/etc/nginx/conf.d/application.conf' do
+	   source 'application.conf.erb'
+	   action :create
+	end
+
+
+log "Remove default sites enabled file"
+
+        file '/etc/nginx/sites-enabled/default' do
+	    action :delete
+	    only_if { File.exist? '/etc/nginx/sites-enabled/default' }
+	end
+		
+		
+log "Restart Nginx"
+		
+	execute 'Restart Nginx' do
+	    command 'sudo sudo service nginx restart'
+        end		
+		
