2d48fb54a06032dc63b66605f967e87f1a4cf378
Harsh Chowdhary <iharsh.c@gmail.com>
2016-07-21 11:58:14 +0000

    nginx default.rb modified

--- cookbooks/golang/recipes/default.rb
+++ cookbooks/golang/recipes/default.rb
@@ -2,7 +2,37 @@
 # Cookbook Name:: golang
 # Recipe:: default
 #
-# Copyright 2016, YOUR_COMPANY_NAME
+# Copyright 2016, Harsh Chowdhary
 #
 # All rights reserved - Do Not Redistribute
 #
+
+log "Installing Golang Package"
+
+        package 'golang' do
+           action :install
+        end
+
+
+log "Installing Git Package"
+
+
+        package 'git' do
+           action :install
+        end
+
+log "Checking out git repository for the sample Go app"
+
+        git "/home/vagrant/sbgoapp" do
+            repository "https://github.com/harsh-tcs/sbgoapp.git"
+			revision "master"
+            action :sync
+        end
+
+
+log "Compiling and Running the Go Application"
+
+        execute 'SB Go App' do
+            command 'sudo go run /home/vagrant/sbgoapp/sampleapp.go &'
+        end
+
