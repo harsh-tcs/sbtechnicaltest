d793088be5eb09783282a639b2f1b5bf62b83e1f
Harsh Chowdhary <harsh.chowdhary05@gmail.com>
2016-07-21 19:49:54 +0530

    Update metadata.rb

--- cookbooks/nginx/metadata.rb
+++ cookbooks/nginx/metadata.rb
@@ -1,7 +1,7 @@
 name             'nginx'
-maintainer       'YOUR_COMPANY_NAME'
+maintainer       'Harsh Chowdhary -  TCS'
 maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
-description      'Installs/Configures nginx'
+description      'Installs and Configures nginx for load balancing app servers in Round Robin fashion'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
